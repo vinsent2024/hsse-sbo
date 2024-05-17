d62cf1bf717bcc006827a4ec2d47cf16b80c5e1a
vinsent2024 <168044144+vinsent2024@users.noreply.github.com>
2024-05-17 15:40:11 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -96,3 +96,30 @@ document.addEventListener("DOMContentLoaded", function() {
       }
   });
 });
+
+
+ // Get the modal
+ var modal = document.getElementById("policy-modal");
+
+ // Get the button that opens the modal
+ var button = document.getElementById("terms-button");
+
+ // Get the <span> element that closes the modal
+ var span = document.getElementsByClassName("close-button")[0];
+
+ // When the user clicks the button, open the modal 
+ button.onclick = function() {
+   modal.style.display = "block";
+ }
+
+ // When the user clicks on <span> (x), close the modal
+ span.onclick = function() {
+   modal.style.display = "none";
+ }
+
+ // When the user clicks anywhere outside of the modal, close it
+ window.onclick = function(event) {
+   if (event.target == modal) {
+     modal.style.display = "none";
+   }
+ }
