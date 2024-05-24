e9e4b3e7a247805d10173182921ee771574de440
vinsent2024 <168044144+vinsent2024@users.noreply.github.com>
2024-05-24 09:58:47 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -124,23 +124,3 @@ document.addEventListener("DOMContentLoaded", function() {
    }
  }
 
- //report modal
- document.addEventListener("DOMContentLoaded", function() {
-  var modal = document.getElementById("myModal");
-  var btn = document.getElementById("openModalBtn");
-  var span = document.getElementsByClassName("close-btn")[0];
-
-  btn.onclick = function() {
-      modal.style.display = "block";
-  }
-
-  span.onclick = function() {
-      modal.style.display = "none";
-  }
-
-  window.onclick = function(event) {
-      if (event.target == modal) {
-          modal.style.display = "none";
-      }
-  }
-});
