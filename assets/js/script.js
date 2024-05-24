0ed901d36b4db2507e6762296dd502b70f8ed367
vinsent2024 <168044144+vinsent2024@users.noreply.github.com>
2024-05-24 10:44:39 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
 document.getElementById('dark-mode-toggle').addEventListener('click', function() {
     document.body.classList.toggle('dark-mode');
   });
+
+  const button = document.getElementById('dark-mode-toggle');
+  let darkMode = false;
+
+  button.addEventListener('click', () => {
+    darkMode = !darkMode;
+    if (darkMode) {
+      button.innerHTML = '<i class="fas fa-moon"></i>Dark Theme';
+      // Add dark mode styles or toggle a dark mode class on body or other elements
+      document.body.style.backgroundColor = '#222';
+      document.body.style.color = '#fff';
+    } else {
+      button.innerHTML = '<i class="fas fa-moon"></i>Dark Mode';
+      // Remove dark mode styles or toggle a dark mode class on body or other elements
+      document.body.style.backgroundColor = '#fff';
+      document.body.style.color = '#000';
+    }
+  });
