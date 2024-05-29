390106bd231442fdba491e0f41d1edec7412093b
vinsent2024 <168044144+vinsent2024@users.noreply.github.com>
2024-05-29 09:38:43 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -124,3 +124,14 @@ document.addEventListener("DOMContentLoaded", function() {
    }
  }
 
+//pwa
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, error => {
+        console.log('ServiceWorker registration failed: ', error);
+      });
+  });
+}
