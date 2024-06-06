447141012b8158bd146f33e0725a1efdd3609638
vinsent2024 <168044144+vinsent2024@users.noreply.github.com>
2024-06-05 23:10:50 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,24 @@
 const cacheName = 'sbo-v1';
+const resourcesToPrecache = [
+  '/',
+  'index.html',
+  'assets/css/style.css',
+  'assets/img/favicon.png',
+  // add more resources as needed
+];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(cacheName)
-      .then(cache => cache.addAll([
-        '/',
-        'index.html',
-        'assets/css/style.css',
-        'assets/img/favicon.png',
-        // add more resources as needed
-      ]))
+      .then(cache => cache.addAll(resourcesToPrecache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.open(cacheName)
-      .then(cache => cache.match(event.request))
+    caches.match(event.request)
       .then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-
-        return fetch(event.request)
-          .then(response => {
-            // Cache responses with Cache-Control: max-age or Cache-Control: immutable
-            if (response && (response.headers.get('Cache-Control').includes('max-age=') || response.headers.get('Cache-Control').includes('immutable'))) {
-              caches.open(cacheName)
-                .then(cache => {
-                  cache.put(event.request, response.clone());
-                });
-            }
-            return response;
-          });
+        return cachedResponse || fetch(event.request);
       })
   );
 });
