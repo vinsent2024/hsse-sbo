2e007d1b043fc5e809b6f2f5b973f4e2a2b2119e
vinsent2024 <168044144+vinsent2024@users.noreply.github.com>
2024-07-02 16:36:20 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,11 +35,18 @@ document.getElementById('dark-mode-toggle').addEventListener('click', function()
       const cards = document.querySelectorAll('.card');
       const noResults = document.getElementById('no-results');
       const userInputSpan = document.getElementById('userinput');
-
+    
       input.addEventListener('input', function() {
         const filter = input.value.toLowerCase();
+        
+        // Check if the input is "breaktime" and redirect if true
+        if (filter === 'breaktime') {
+          window.location.href = 'trex.html';
+          return; // Exit the function after redirection
+        }
+    
         let hasMatch = false;
-
+    
         cards.forEach(card => {
           const text = card.querySelector('p').textContent.toLowerCase();
           if (text.includes(filter)) {
@@ -49,7 +56,7 @@ document.getElementById('dark-mode-toggle').addEventListener('click', function()
             card.style.display = 'none';
           }
         });
-
+    
         if (hasMatch) {
           noResults.style.display = 'none';
         } else {
@@ -58,3 +65,4 @@ document.getElementById('dark-mode-toggle').addEventListener('click', function()
         }
       });
     });
+    
